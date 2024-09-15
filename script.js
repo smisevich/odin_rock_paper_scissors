35c7ebeaf7749583967e0dcf4ad78e4919f02d08
Stanley Misevich <stanmisevich@gmail.com>
2024-09-15 12:07:36 -0400

    Complete console functionality

--- script.js
+++ script.js
@@ -23,17 +23,86 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function isUserInput() {
-  return roundOptions.includes(userChoice);
+function isUserInput(userChoice) {
+  return roundOptions.includes(userChoice.trim().toLowerCase());
 }
 
 function getUserChoice() {
   do {
     userChoice = prompt("Do you choose 'rock', 'paper', or 'scissors'?");
-    if (isUserInput) {
-      return userChoice.toLowerCase().trim();
+    if (isUserInput(userChoice)) {
+      userChoice = userChoice.trim().toLowerCase();
+      return userChoice;
     } else {
       console.log("Please enter a valid choice.");
     }
-  } while (!isUserInput);
+  } while (!isUserInput(userChoice));
 }
+
+function getResult() {
+  if (userChoice == "rock") {
+    switch (computerChoice) {
+      case "rock":
+        return roundResults[2];
+        break;
+      case "paper":
+        computerScore++;
+        return roundResults[1];
+        break;
+      case "scissors":
+        humanScore++;
+        return roundResults[0];
+        break;
+    }
+  } else if (userChoice == "paper") {
+    switch (computerChoice) {
+      case "rock":
+        humanScore++;
+        return roundResults[0];
+        break;
+      case "paper":
+        return roundResults[2];
+        break;
+      case "scissors":
+        computerScore++;
+        return roundResults[1];
+        break;
+    }
+  } else {
+    switch (computerChoice) {
+      case "rock":
+        computerScore++;
+        return roundResults[1];
+        break;
+      case "paper":
+        humanScore++;
+        return roundResults[0];
+        break;
+      case "scissors":
+        return roundResults[2];
+        break;
+    }
+  }
+}
+
+function playRound(computerChoice, userChoice) {
+  const result = getResult();
+  console.log(`You choose: ${userChoice}`);
+  console.log(`The computer chose ${computerChoice}. ${result}`);
+  console.log(`Your Score: ${humanScore} -- Computer Score: ${computerScore}`);
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    playRound(getComputerChoice(), getUserChoice());
+  }
+  if (humanScore > computerScore) {
+    console.log("Final result out of 5 rounds: You win!");
+  } else if (humanScore < computerScore) {
+    console.log("Final result out of 5 rounds: You lose!");
+  } else {
+    console.log("Final result out of 5 rounds: It's a draw!");
+  }
+}
+
+playGame();
